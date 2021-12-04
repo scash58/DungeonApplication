45df71ea322d8e8e274271acc545e6f2b2d1b8c6
Scott Cashion <scashion58@outlook.com>
2021-12-04 12:14:00 -0600

    Changed File Path and added DungeonLibrary and DungeonMonster

--- DungeonApplication/Program.cs
+++ DungeonApplication/Program.cs
@@ -10,6 +10,17 @@ namespace DungeonApplication
     {
         static void Main(string[] args)
         {
+            //TODO 01 Create a character class
+            //TODO 02 Create a player class maybe player enemy
+            //TODO 03 Create monster class and subclasses
+            //TODO 04 Create weapon class, subclasses
+            //TODO 05 Create armor class, subclasses
+            //TODO 06 Create a store
+            //TODO 07 Create character archtypes: warrior barbarian knight
+            //TODO 08 Create a Dungeon room w/monster, loot, ect.
+            //TODO 09 Make interface for the Dungeon
+            //TODO 10 Do combat
+            //TODO 11 make a scoring/experience system
         }
     }
 }
